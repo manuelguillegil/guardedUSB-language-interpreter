805d901cd3104fdf9435810ce5878335114f5ec0
Diego <djpg98@gmail.com>
2019-10-05 19:38:27 -0400

    Ya da las columns bien en la mayoría de los casos. Cuando hay error logré que imprimiera solo los errores, como debe ser. Lee el input del archivo. Falta arreglar lo de los tabs, si es que se puede arreglar

--- ply-3.11/lexer.py
+++ ply-3.11/lexer.py
@@ -3,139 +3,195 @@
 ##Manuel Gil, 14-10397
 ##Diego Peña, 15-11095
 ##Fecha de inicio: 28-09-2019, 19:44 Hora de Venezuela
-##Fecha de modificación: 05-10-2019, 09:00 Hora de Venezuela
+##Fecha de modificación: 05-10-2019, 19:31 Hora de Venezuela
 
 #Comentarios generales: Antlr es muy complicado para aprender en una semana. Viva Python y Ply
 
-#Últimas modificaciones: Añadí las últimas palabras reservadas que faltaban. Arreglé el output del print en el tokeneizer
-#
+#Últimas modificaciones: Ya lee el input de un archivo. Calcula correctamente el número de columna en un caso
+#de output normal siempre y cuando no haya tabs en las líneas. Da error si no tiene archivo de input o si este no
+#tiene extensión .gusb. Ya da mensajes de error
+
+#Bugs
+#No da bien el número de columnas cuando hay tabs
 
 #To Do: 
-# Falta añadir en que columna se produjo el error en la función de t_error.
-# Hay un error en la función de error, por eso la comenté, pero hay que ver que es y arreglarlo
-# Falta hacer que lea el input de un archivo directamente
+# Arreglar el bug
 
-#Esto es lo que me dice un video en internet que debo importar, pero no se cuales hagan falta al final
 import ply.lex as lex #Luthor
 import re
-import codecs
 import sys
-import os
+import os #No se si esta va
 
-tokens = ['TkOBlock', 'TkCBlock', 'TkSoForth', 'TkComma', 'TkOpenPar', 'TkClosePar', 'TkAsig', 
-        'TkSemicolon', 'TkArrow', 'TkPlus', 'TkMinus', 'TkMult', 'TkDiv', 'TkMod', 'TkOr', 
-        'TkAnd', 'TkNot', 'TkLess', 'TkLeq', 'TkGeq', 'TkGreater', 'TkEqual', 'TkNequal', 
-        'TkOBracket', 'TkCBracket', 'TkTwoPoints', 'TkConcat', 'TkNum', 'TkId']
+class CustomLexer(object):
 
-reservadas = {
-    'if':   'TkIf',
-    'fi':   'TkFi',
-    'do':   'TkDo',
-    'od':   'TkOd',
-    'bool': 'TkBool',
-    'int':  'TkInt',
-    'array':'TkArray',
-    'declare':'TkDeclare',
-    'println':'TkPrintln',
-    'print':'TkPrint',
-    'for':'TkFor',
-    'in':'TkIn',
-    'to':'TkTo',
-    'rof':'TkRof',
-    'read':'Tkread',
-    'count':'TkCount',
-    'atoi':'TkAtoi',
-    'size':'TkSize',
-    'max':'TkMax',
-    'min':'TkMin'
-}
- 
-# Una cadena que contiene caracteres ignorados (espacios y tabulaciones) 
-t_ignore = ' \t' 
-t_TkOBlock = r'\|\['
-t_TkCBlock = r'\]\|'
-t_TkSoForth = r'\.\.'
-t_TkComma = r','
-t_TkOpenPar = r'\('
-t_TkClosePar = r'\)'
-t_TkAsig = r':='
-t_TkSemicolon = r';'
-t_TkArrow = r'\-\->'
-t_TkPlus = r'\+'
-t_TkMinus = r'\-'
-t_TkMult = r'\*'
-t_TkDiv = r'/'
-t_TkMod = r'%'
-t_TkOr = r'\\/'
-t_TkAnd = r'/\\'
-t_TkNot = r'!'
-t_TkLess = r'<'
-t_TkLeq = r'<='
-t_TkGeq = r'>='
-t_TkGreater = r'>'
-t_TkEqual = r'=='
-t_TkNequal = r'!='
-t_TkOBracket = r'\['
-t_TkCBracket = r'\]'
-t_TkTwoPoints = r':'
-t_TkConcat = r'\|\|'
+    errors = 0
 
-# Concatenamos los tokens y las palabras reservadas
-tokens = tokens + list(reservadas.values())
+    tokens = ['TkOBlock', 'TkCBlock', 'TkSoForth', 'TkComma', 'TkOpenPar', 'TkClosePar', 'TkAsig', 
+            'TkSemicolon', 'TkArrow', 'TkPlus', 'TkMinus', 'TkMult', 'TkDiv', 'TkMod', 'TkOr', 
+            'TkAnd', 'TkNot', 'TkLess', 'TkLeq', 'TkGeq', 'TkGreater', 'TkEqual', 'TkNequal', 
+            'TkOBracket', 'TkCBracket', 'TkTwoPoints', 'TkConcat', 'TkIdError', 'TkNum', 'TkId']
 
-def t_TkNum(t):
-    r'\d+'
-    t.value = int(t.value)
-    return t
+    reservadas = {
+        'if':   'TkIf',
+        'fi':   'TkFi',
+        'do':   'TkDo',
+        'od':   'TkOd',
+        'bool': 'TkBool',
+        'int':  'TkInt',
+        'array':'TkArray',
+        'declare':'TkDeclare',
+        'println':'TkPrintln',
+        'print':'TkPrint',
+        'for':'TkFor',
+        'in':'TkIn',
+        'to':'TkTo',
+        'rof':'TkRof',
+        'read':'Tkread',
+        'count':'TkCount',
+        'atoi':'TkAtoi',
+        'size':'TkSize',
+        'max':'TkMax',
+        'min':'TkMin'
+    }
+    
+    # Una cadena que contiene caracteres ignorados (espacios y tabulaciones) 
+    t_ignore = ' \t' 
+    t_TkOBlock = r'\|\['
+    t_TkCBlock = r'\]\|'
+    t_TkSoForth = r'\.\.'
+    t_TkComma = r','
+    t_TkOpenPar = r'\('
+    t_TkClosePar = r'\)'
+    t_TkAsig = r':='
+    t_TkSemicolon = r';'
+    t_TkArrow = r'\-\->'
+    t_TkPlus = r'\+'
+    t_TkMinus = r'\-'
+    t_TkMult = r'\*'
+    t_TkDiv = r'/'
+    t_TkMod = r'%'
+    t_TkOr = r'\\/'
+    t_TkAnd = r'/\\'
+    t_TkNot = r'!'
+    t_TkLess = r'<'
+    t_TkLeq = r'<='
+    t_TkGeq = r'>='
+    t_TkGreater = r'>'
+    t_TkEqual = r'=='
+    t_TkNequal = r'!='
+    t_TkOBracket = r'\['
+    t_TkCBracket = r'\]'
+    t_TkTwoPoints = r':'
+    t_TkConcat = r'\|\|'
 
-def t_TkId(t):
-    r'[a-zA-Z_][a-zA-Z0-9_]*'
-    t.type = reservadas.get(t.value,'TkId') 
-    return t
+    # Concatenamos los tokens y las palabras reservadas
+    tokens = tokens + list(reservadas.values())
 
-# El lexer identifica los comentarios, pero no hace nada ya que no es necesario guardar el token
-def t_COMMENT(t):
-    r'//.*'
-    pass
+    def t_TkIdError(self, t):
+        r'\d+[a-zA-Z_][a-zA-Z0-9_]*'
+        self.t_error(t)
 
-# Calcular columna. 
-# input es la cadena de texto de entrada 
-# token es una instancia de token 
-def find_column(input, token):
-    line_start = input.rfind('\n', 0, token.lexpos) + 1
-    return (token.lexpos - line_start) + 1
 
-def find_line(t):
-    t.lexer.lineno += t.value.count('\n')
-    return t.lexer.lineno
+    def t_TkNum(self, t):
+        r'\d+'
+        t.value = int(t.value)
+        return t
 
-# Regla para que podamos rastrear los números de línea
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
+    def t_TkId(self, t):
+        r'[a-zA-Z_][a-zA-Z0-9_]*'
+        t.type = self.reservadas.get(t.value,'TkId') 
+        return t
 
-#Función cuando existe un error en el token
-def t_error(t):
-    #print("Error: Unexpected character " + str(t.value[0]) + "in row " + str(t.lineno]) +  ", column ")
-    t.lexer.skip (1)
+    # El lexer identifica los comentarios, pero no hace nada ya que no es necesario guardar el token
+    def t_COMMENT(self, t):
+        r'//.*'
+        pass
 
+    # token es una instancia de token tal que queremos calcular la lexpos donde empieza la línea de ese token
+    # start es el lexpos donde empezaba la línea anterior a la de este token
+    # return: La lexpos donde empieza la línea que contiene al token
+    def find_line_start(self, token, start):
+        line_start = self.lexer.lexdata.rfind('\n', start, token.lexpos) + 1
+        return line_start
+
+    def find_line(self, t):
+        t.lexer.lineno += t.value.count('\n')
+        return t.lexer.lineno
+
+    # Regla para que podamos rastrear los números de línea
+    def t_newline(self, t):
+        r'\n+'
+        t.lexer.lineno += len(t.value)
+
+    #Función cuando existe un error en el token
+    def t_error(self, t):
+        col = t.lexpos - self.find_line_start(t, 0) + 1
+        print("Error: Unexpected character " + str(t.value[0]) + " in row " + str(t.lineno) +  ", column " + str(col))
+        self.errors += 1
+        t.lexer.skip (1)
+
+    def build(self,**kwargs):
+         self.lexer = lex.lex(module=self, **kwargs)
+
+    def analysis(self, text):
+        self.lexer.input(text)
+
+        #prevLine guarda el número de la última línea leída
+        #lineStart representa la lexpos donde empieza la línea del token que estamos imprimiendo. Al inicio está en cero
+        #porque esa es la menor lexpos posible
+        prevLine = 0
+        lineStart = 0
+        output = ""
+
+        while True:
+
+            tok = self.lexer.token()
+            
+            if not tok: 
+                break
+
+            #Si la última línea leída es distinta de la actual, cambiamos de línea y hay que calcular donde
+            #empieza esta nueva línea para determinar la columna de los tokens en esta nueva línea
+            if prevLine != tok.lineno:
+                lineStart = self.find_line_start(tok, lineStart)
+                prevLine = tok.lineno
+
+            #Usando la lexpos del token actual y la lexpos donde comienza la línea, obtenemos la columna del token
+            col = tok.lexpos - lineStart + 1
+
+            if tok.type == 'TkId':
+                output += '%s(%r) %d %d\n' % (tok.type, tok.value, tok.lineno, col)
+            elif tok.type == 'TkNum':
+                output += '%s("%r") %d %d\n' % (tok.type, tok.value, tok.lineno, col)
+            else:
+                output += '%s %d %d\n' % (tok.type, tok.lineno, col)
+
+        if self.errors > 0:
+            print("Total errors: " + str(self.errors))
+        else:
+            print(output)
 
 ############## MAIN #################### Deberíamos ponerlo en otro archivo
 
+#Nos aseguramos de que haya un arcghivo de input
+try:
+    assert(len(sys.argv) > 1)
+except:
+    print("No hay archivo de input")
+    sys.exit()
+
+#Nos aseguramos de que el archivo input tenga la extensión requerida
+try:
+    assert('.gusb' in sys.argv[1])
+except:
+    print("Este tipo de archivo no es reconocido")
+    sys.exit()
+
 # Construir el lexer
-lexer = lex.lex()
+lexer = CustomLexer()
+lexer.build()
 
-# Hacer un input al lexer
-lexer.input("int x := 5; if x == 5 --> x := 6; fi")
+with open(sys.argv[1]) as fp:
+    lexer.analysis(fp.read())
+fp.closed
 
-# Tokenize
-while True:
-    tok = lexer.token()
-    if not tok: 
-        break      # No more input
-    if tok.type == 'TkId':
-        print('%s(%r) %d %d' % (tok.type, tok.value, tok.lineno, tok.lexpos))
-    elif tok.type == 'TkNum':
-        print('%s("%r") %d %d' % (tok.type, tok.value, tok.lineno, tok.lexpos))
-    else:
-        print('%s %d %d ' % (tok.type, tok.lineno, tok.lexpos))
